c05566395f0e47a1cd7f063dae79488b718bd29d
Daniel Tome Fernandez <danieltomefer@gmail.com>
2019-10-14 20:39:35 +0200

    Added startbootstrap template

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+import '@fortawesome/fontawesome-free/js/fontawesome.min';
+import 'startbootstrap-freelancer/vendor/jquery/jquery.min';
+import 'startbootstrap-freelancer/vendor/jquery-easing/jquery.easing.min';
+import 'startbootstrap-freelancer/vendor/bootstrap/js/bootstrap.bundle.min';
+
 const name = 'world';
-console.log(`Hello ${name}`);
+console.log(`Hello ${name} 2` );
